108a13b86cd32efa431cf3f880b145ed50a870a1
abhay-tank <abhay.tank@raweng.com>
2020-12-21 03:11:58 +0530

    Removed multer from auth

--- src/api-v1/middlewares/uploadUserAvatarImage.js
+++ src/api-v1/middlewares/uploadUserAvatarImage.js
@@ -5,71 +5,31 @@ const { config } = require("../configuration/config");
 const ErrorResponse = require("../models/ErrorResponse");
 const sendErrorResponse = require("./responses/sendErrorResponse");
 
-const fileFilter = (req, file, cb) => {
-  const validFileTypes = ["image/gif", "image/png", "image/jpeg", "image/jpg"];
-  if (validFileTypes.includes(file.mimetype)) {
-    cb(null, true);
-  } else {
-    cb(new ErrorResponse(400, "unsuccessful", "Invalid file type."), false);
-  }
-};
-
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    const userPicDirectory = path.join(
-      __dirname,
-      "..",
-      "..",
-      "..",
-      config.RESOURCES,
-      config.USER_IMAGES,
-      req.headers.userId
-    );
-    const dirExists = fs.existsSync(userPicDirectory);
-    if (!dirExists) {
-      fs.mkdirSync(userPicDirectory);
-    }
-    return cb(null, userPicDirectory);
-  },
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + "-" + file.originalname);
-  },
+	destination: (req, file, cb) => {
+		const userPicDirectory = path.join(
+			__dirname,
+			"..",
+			"..",
+			"..",
+			config.RESOURCES,
+			config.USER_IMAGES,
+			req.headers.userId
+		);
+		const dirExists = fs.existsSync(userPicDirectory);
+		if (!dirExists) {
+			fs.mkdirSync(userPicDirectory);
+		}
+		return cb(null, userPicDirectory);
+	},
+	filename: (req, file, cb) => {
+		cb(null, Date.now() + "-" + file.originalname);
+	},
 });
 
 const uploadUserAvatarImage = multer({
-  storage: storage,
-  limits: { fileSize: 1024 * 1024 * 10 },
-  fileFilter: fileFilter,
-}).single("avatarImage");
+	storage: storage,
+	limits: { fileSize: 1024 * 1024 * 10 },
+});
 
-const uploadNone = multer().none();
-
-const userAvatarUpload = (req, res, next) => {
-  if (req.path == "/signIn") {
-    uploadNone(req, res, (err) => {
-      next();
-    });
-  } else {
-    uploadUserAvatarImage(req, res, (err) => {
-      if (err instanceof multer.MulterError) {
-        console.error(err);
-        return sendErrorResponse(
-          new ErrorResponse(400, "unsuccessful", "Error uploading images"),
-          res
-        );
-      } else if (err instanceof ErrorResponse) {
-        console.error(err);
-        return sendErrorResponse(err, res);
-      } else if (err instanceof Error) {
-        console.error(err);
-        return sendErrorResponse(
-          new ErrorResponse(500, "unsuccessful", "Error uploading images"),
-          res
-        );
-      }
-      next();
-    });
-  }
-};
-
-module.exports = userAvatarUpload;
+module.exports = { uploadUserAvatarImage };
--- src/api-v1/routes/authRoutes.js
+++ src/api-v1/routes/authRoutes.js
@@ -1,24 +1,33 @@
 const express = require("express");
 const authRouter = express.Router();
 const createUserId = require("../middlewares/createUserId");
-const userAvatarUpload = require("../middlewares/uploadUserAvatarImage");
 const {
-  signUp,
-  signIn,
-  signOut,
-  verifyUserAccount,
-  requestVerificationEmail,
+	uploadUserAvatarImage,
+} = require("../middlewares/uploadUserAvatarImage");
+const {
+	signUp,
+	signIn,
+	signOut,
+	verifyUserAccount,
+	requestVerificationEmail,
 } = require("../controllers/authController");
 const authIsUserSignedIn = require("../middlewares/authIsUserSignedIn");
 const protectRoute = require("../middlewares/protectRoute");
 authRouter
-  .route("/signUp")
-  .post(authIsUserSignedIn, createUserId, userAvatarUpload, signUp);
-authRouter.route("/signIn").post(authIsUserSignedIn, userAvatarUpload, signIn);
+	.route("/signUp")
+	.post(
+		authIsUserSignedIn,
+		createUserId,
+		uploadUserAvatarImage.single("avatarImage"),
+		signUp
+	);
+authRouter
+	.route("/signIn")
+	.post(authIsUserSignedIn, uploadUserAvatarImage.none(), signIn);
 authRouter.route("/signOut").get(protectRoute, signOut);
 authRouter
-  .route("/verifyUserAccount/:verificationToken")
-  .get(verifyUserAccount);
+	.route("/verifyUserAccount/:verificationToken")
+	.get(verifyUserAccount);
 authRouter.route("/sendVerificationEmail/:id").get(requestVerificationEmail);
 
 module.exports = authRouter;
