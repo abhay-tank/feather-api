66c66df4755937044001f205228a4a058ee7da7d
abhay-tank <abhay.tank@raweng.com>
2021-01-10 22:29:17 +0530

    removed local storage for images and its configurations

--- app.js
+++ app.js
@@ -28,7 +28,6 @@ mongoose.connect(
 		app.use(compression());
 		app.use(helmet());
 		app.use(express.json());
-		app.use(express.static(config.RESOURCES));
 		app.use("/auth", authRouter);
 		app.use("/blogs", blogsRouter);
 		app.use((req, res, next) => {
--- src/api-v1/configuration/config.js
+++ src/api-v1/configuration/config.js
@@ -2,7 +2,6 @@ const config = {
 	PORT: process.env.PORT,
 	DATABASE_URL: process.env.DATABASE_URL,
 	JWT_SECRET: process.env.JWT_SECRET,
-	RESOURCES: process.env.RESOURCES,
 	BLOG_IMAGES: process.env.BLOG_IMAGES,
 	USER_IMAGES: process.env.USER_IMAGES,
 	NODE_MAILER_EMAIL: process.env.NODE_MAILER_EMAIL,
